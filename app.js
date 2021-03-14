c086478483d8540b40f846302c706dcc316bc63e
Baiju Joseph <baiju.joseph@gmail.com>
2021-03-14 15:30:39 +0000

    Basic model structure created with Sequelize-CLI

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
+
 
 const app = express();
 
 app.use(logger('dev'));
 
-app.get('/', (req, res) => {
-    res.send(`<h1>We are live</h1>`);
-})
 
-PORT = 3001;
+PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
